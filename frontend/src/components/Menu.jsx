58a343b2152058ee0d1e948703d8f2b17197729b
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-17 22:30:54 +0300

    made changes

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 import styled from 'styled-components'
 import logoimg from '../assets/logo.svg'
 import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import HistoryIcon from '@mui/icons-material/History';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import './menucss.css'
+
 const Container = styled.div`
-    flex:1;
+    flex:1.3;
     background: #202020;
     height: 100vh;
     color: white;
@@ -29,12 +36,23 @@ const Item = styled.div`
     align-items: center;
     gap: 20px;
     cursor: pointer;
+    padding: 7px 0;
 
     &:hover{
         color: #3ac6a6;
     }
 `
+const Login = styled.div`
 
+`
+const Loginbtn = styled.button`
+    background-color: transparent;
+    color: white;
+    padding: 5px 10px;
+    border: 1px solid #3ac6a6;
+    cursor: pointer;
+    font-weight: 500;
+`
 
 function Menu() {
   return (
@@ -42,12 +60,38 @@ function Menu() {
        <Wrapper>
         <Logo>
             <Img src={logoimg}/>
-            <h2>Vider</h2>
+            <h2>Vidlit</h2>
         </Logo>
         <Item>
             <HomeIcon />
             Home
         </Item>
+        <hr style={{border:'0.5px solid #373737'}} />
+        <Item>
+            <VideoLibraryIcon />
+            My videos
+        </Item>
+        <Item>
+            <AccessTimeIcon />
+            Watch later
+        </Item>
+        <Item>
+            <FavoriteBorderIcon />
+            Liked videos
+        </Item>
+        <Item>
+            <HistoryIcon />
+            History
+        </Item>
+        <Item>
+            <SettingsIcon />
+            Settings
+        </Item>
+        <hr style={{border:'0.5px solid #373737'}} />
+        <Login>
+            Sign in to like videos and comment
+            <Loginbtn>Sign in</Loginbtn>
+        </Login>
        </Wrapper>
     </Container>
   );
