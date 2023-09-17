744a0299807aca66175c31f871096874a6100296
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-17 18:24:08 +0300

    project start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
 import './App.css'
+import styled from 'styled-components'
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+  display:flex;
+
+`
+const Main = styled.div`
+  flex:7
+`
+const Body = styled.div``
 
 function App() {
 
   return (
-    <div>Hello world</div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Body>
+          vids
+        </Body>
+      </Main>
+    </Container>
   );
 }
 
---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    flex:1;
+    background: #202020
+`
+
+function Menu() {
+  return (
+    <Container>Menu</Container>
+  );
+}
+
+export default Menu
