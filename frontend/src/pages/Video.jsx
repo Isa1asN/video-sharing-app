364cb5908503f15388ab49c76945c34e2bd3c271
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-18 15:16:32 +0300

    made changes

--- frontend/src/pages/Video.jsx
+++ frontend/src/pages/Video.jsx
@@ -40,24 +40,51 @@ const Buttons = styled.div`
   color: ${({theme}) => theme.text};
 `
 const Button = styled.div`
-  
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 `
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const ChannelInfo = styled.div``
+const Image = styled.img``
+const ChannelDetail = styled.div``
+const ChannelName = styled.span``
+const ChannelCounter = styled.span``
+const Description = styled.p``
+
+
 function Video() {
   return (
     <Container>
       <Content>
         <VideoWrapper>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/u9n7Cw-4_HQ?si=E0laqkAeVqxhTuWz" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="90%" height="360" src="https://www.youtube.com/embed/u9n7Cw-4_HQ?si=E0laqkAeVqxhTuWz" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </VideoWrapper>
         <Title>Test Video- The Weeknd</Title>
         <Details>
           <Info>7,001 views | Jul 2022</Info>
           <Buttons>
-            <Button><FavoriteIcon /> 12</Button>
+            <Button><FavoriteIcon /> <small>12</small></Button>
             <Button><ShareIcon /></Button>
-            <Button><DownloadIcon /> 176</Button>
+            <Button><DownloadIcon /> <small>163</small></Button>
           </Buttons>
         </Details>
+        <hr />
+        <Channel>
+          <ChannelInfo>
+            <Image />
+            <ChannelDetail>
+              <ChannelName>Esu presents</ChannelName>
+              {/* <ChannelCounter>3002 followers</ChannelCounter> */}
+              <Description>
+                Lorem ipsum dolor lore m ipsu m 
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+        </Channel>
       </Content>
       <Recommendation>
         recommendation
