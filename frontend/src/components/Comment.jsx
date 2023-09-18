ceb4204afb4ddf39c4f34f8cad9fa632a824a18b
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-18 18:56:22 +0300

    added the comment section

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,7 +1,51 @@
+import styled from "styled-components"
 
+const Container = styled.div`
+    display: flex;
+    gap: 10px;
+    margin: 30px 0px;
+`
+const Avatar = styled.img`
+height: 50px;
+width: 50px;
+border-radius: 50%;
+background-color: #231ddc;
+`
+const Details = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    color: ${({theme}) => theme.text};
+`
+const Name = styled.span`
+    font-size: 13px;
+    font-weight: 500;
+    
+`
+const Date = styled.span`
+    font-size: 12px;
+    font-weight: 400;
+    margin-left: 5px;
+    
+`
+const Text = styled.span`
+    font-size: 14px;
+    
+`
 function Comment() {
   return (
-    <div>Comment</div>
+    <Container>
+        <Avatar />
+        <Details>
+            <Name>
+                John Doe
+                <Date>2 days ago</Date>
+            </Name>
+            <Text>
+                Lorem ipsum dolor oidho donn  on o
+            </Text>
+        </Details>
+    </Container>
   )
 }
 
