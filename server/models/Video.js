79b21dbdb540cfdd616333f34ae5375a0a238341
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-21 08:20:51 +0300

    added the user model

---
+++ server/models/Video.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Videoschema = new mongoose.Schema(
+    {
+    name : {
+        type : String,
+        required: true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required: true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required: true,
+    },
+    img : {type : String},
+    followers : {
+        type : Number,
+        default : 0
+    },
+    following : {
+        type : [String]
+    },
+    timestamps : true
+}
+)
+const User = mongoose.model('Users', Userschema)
+export default User;
