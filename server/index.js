65b806ef969257dc72114b4cb32263125e05a2a5
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-21 07:55:00 +0300

    made some changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
 
-app.listen(8000, () => {
-    console.log('Connected!')
-})
+const port = process.env.PORT || 8000
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {app.listen(port, () => 
+    console.log(`Connected! server running on port ${port}`))})
+    .catch((err) => console.log(`${err} Did not connect`))  
