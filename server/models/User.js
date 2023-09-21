718fc3c4a9709efda477b6c4069c8a44c4f5c81b
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-21 08:16:55 +0300

    added the user model

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Userschema = new mongoose.Schema(
+    {
+    name : {
+        type : String,
+        required: true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required: true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required: true,
+    },
+    img : {type : String},
+    followers : {
+        type : Number,
+        default : 0
+    },
+    following : {
+        type : [String]
+    },
+    timestamps : true
+}
+)
+mongoose.model('Users', Userschema)
