d3421b8c6c6be90d962ee7e3f88edc1df2bef5b6
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-22 09:25:21 +0300

    made some changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,16 +3,18 @@ import User from '../models/User.js'
 
 
 export const signUp = async (req, res) => {
-    const existUser = await User.findOne({email : req.body.email})
-    if (existUser) return res.status(403).json({error : 'email already exists'});
+    const existEmail = await User.findOne({email : req.body.email})
+    const existName = await User.findOne({name : req.body.name})
+    if (existEmail) return res.status(403).json({error : 'Email already exists'});
+    if (existName) return res.status(403).json({error : 'Name already exists'});
     const user = new User(req.body);
     const salt = await bcrypt.genSalt(10);
     const hashedPwrd = await bcrypt.hash(user.password, salt);
     user.password = hashedPwrd;
     try {
         await user.save()
-        res.status(201).json("Successfully signed up");
+        res.status(201).json("User successfully signed up");
     } catch (error) {
-        res.status(400).json({error : error});
+        res.status(500).json({error : error});
     }
 }
