2e6f51e264a0bcabe7e404bdaa677d6ab58d6302
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-22 11:11:28 +0300

    added the signout routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signIn, signUp } from '../controllers/auth.js';
+import { signIn, signUp, signout } from '../controllers/auth.js';
 
 const authRoutes = express.Router()
 
 authRoutes.post("/signup", signUp)
 authRoutes.post("/signin", signIn)
+authRoutes.get("/logout", signout)
 
 export default authRoutes;
