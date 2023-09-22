af0d9b787d1ec96fa2c9f00dd9576f740cae2de7
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-22 09:02:51 +0300

    made some changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,18 @@
+import bcrypt from 'bcrypt'
+import User from '../models/User.js'
 
-export const test = (req, res) =>{
-    console.log("Test is working ")
-    res.send("The test is working")
+
+export const signUp = async (req, res) => {
+    const existUser = await User.findOne({email : req.body.email})
+    if (existUser) return res.status(403).json({error : 'email already exists'});
+    const user = new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPwrd = await bcrypt.hash(user.password, salt);
+    user.password = hashedPwrd;
+    try {
+        await user.save()
+        res.status(201).json("Successfully signed up");
+    } catch (error) {
+        res.status(400).json({error : error});
+    }
 }
-
