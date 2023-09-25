088bdeb5169ed640b95d9fa5336282117206c11f
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-24 21:34:25 -0700

    made changes

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,6 +1,10 @@
+import User from '../models/User.js'
 
 export const test = (req, res) =>{
     console.log("Test is working ")
     res.send("The test is working")
 }
 
+export const getVideos = async (req, res) => {
+    
+}
