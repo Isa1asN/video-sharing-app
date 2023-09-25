e721c29fad6a86980441446d80d90697db02191e
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-24 21:30:59 -0700

    made changes

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyUserToken = async (req, res) => {
+    try {
+        let token = req.headers.authorization;
+        if (!token) {res.status(401).send("Access Denied")}
+
+        token = token.split(' ')[1]
+        if(!token || token===null){res.status(401).send("unauthorized request")}
+
+        let verified = jwt.verify(token, process.env.jwtsecret)
+        if(!verified) {res.status(401).send("unauthorized request")}
+
+        req.user = verified
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(400).send("Invalid Token");
+    }
+    
+}
