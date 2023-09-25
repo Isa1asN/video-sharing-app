4c35e36da0d192945f3cb19f48f1b0f08cf4dce1
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-24 21:43:54 -0700

    added middleware, added the get all videos controller

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,10 +1,19 @@
 import User from '../models/User.js'
+import Video from '../models/Video.js'
 
 export const test = (req, res) =>{
     console.log("Test is working ")
     res.send("The test is working")
 }
 
-export const getVideos = async (req, res) => {
-    
+export const getAllVideos = async (req, res) => {
+        try {
+            let vids = await Video.find({})
+            req.status(200).json(vids)
+            console.log("GET request to get all videos")
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send("an error occured")
+        }
 }
