464e70161acdd00bef44019c70750fa9fe066f57
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-25 16:09:48 -0700

    fixed the bug

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,11 @@
 import User from "../models/User.js"
 
 export const uploadProfileImg = async(req, res) =>{
-    try {
+    try {               
         const userId = req.user._id
-        let profile = User.findByIdAndUpdate(userId, {imgUrl : req.body.imgUrl})
-        res.status(200).json(profile)
+        // console.log("req has arrived")
+        let profile = await User.findByIdAndUpdate(userId, {img : req.body.imgUrl}, {new : true}).select('-password')
+        res.status(200).send(profile)
     } catch (error) {
         console.log(error)
         res.status(500).send("An error occured")
