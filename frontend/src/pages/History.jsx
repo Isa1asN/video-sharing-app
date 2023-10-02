41a8e5a23b2b67a46831353c3702b4f88b862ae0
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-02 16:27:55 -0700

    made changes, issue not fixed

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,63 +1,65 @@
-/* eslint-disable no-unused-vars */
-// import React from 'react'
-import styled from "styled-components"
-import Card from "../components/Card"
-import { useEffect, useState } from "react"
-import axios from 'axios'
-import {useSelector, useDispatch} from 'react-redux'
-import { setHistory, setIsHistory } from "../state/vidSlice"
+// import React from 'react';
+import styled from 'styled-components';
+import Card from '../components/Card';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { setHistory, setIsHistory } from '../state/vidSlice';
 
-const client = axios.create({baseURL : 'http://localhost:3004/api'})
+const client = axios.create({ baseURL: 'http://localhost:3004/api' });
 
 const Container = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap:30px ;
-`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+`;
 
 function History() {
+  const hisV = useSelector((state) => state.video.history);
+  const isHistory = useSelector((state) => state.video.isHistory);
+  const dispatch = useDispatch();
 
-    const hisV = useSelector((state) => state.video.history)
-    const isHistory = useSelector((state) => state.video.isHistory)
-    const dispatch = useDispatch()
-    console.log(hisV)
-    // const history = [...hisV].reverse()
-    useEffect(()=>{
-        const fetchHistory = async () => {
-            try {
-                const response = await client.get('/v/history', {
-                    headers : {
-                        Authorization : `Bearer ${localStorage.getItem('t')}`
-                    }
-                })
-                if (response.status === 200){
-                    dispatch(setIsHistory(true))
-                    dispatch(setHistory(response.data))
-                }
-                else if (response.status === 404) {
-                    // console.log("An error occured or no history")
-                    dispatch(setIsHistory(false))
-                }
-            } catch (error) {
-                console.log(error)
-            }
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const response = await client.get('/v/history', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('t')}`,
+          },
+        });
+        if (response.status === 200) {
+          dispatch(setIsHistory(true));
+          dispatch(setHistory(response.data));
+        } else if (response.status === 404) {
+          dispatch(setIsHistory(false));
         }
-        fetchHistory()
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchHistory();
+  }, [dispatch]);
 
-    }, [dispatch])
+  let history = [...hisV].reverse();
 
-  return (
-        isHistory ?
+  return isHistory ? (
     <Container>
-        {
-        hisV.map((vid) => {
-            return <Card key={vid._id} title={vid.title} userId={vid.userId} views={vid.views} imgUrl={vid.thumbnail} date={vid.createdAt}/>
-        })
-        }
+      {history.map((vid, index) => (
+        <Card
+          key={index} // Using array index as a key, be cautious with this approach
+          title={vid.title}
+          userId={vid.userId}
+          views={vid.views}
+          imgUrl={vid.thumbnail}
+          date={vid.createdAt}
+        />
+      ))}
     </Container>
-    :
-    <center><div>No data to display </div></center>
-  )
+  ) : (
+    <center>
+      <div>No data to display </div>
+    </center>
+  );
 }
 
-export default History
+export default History;
